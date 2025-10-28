47311695da3732be0fd1631f8602bf203e8132dc
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-10-28 11:48:26 +0530

    set routes in server and create folder structure for client side

--- Event-management-system/server/routes/eventRoutes.js
+++ Event-management-system/server/routes/eventRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Events = require('../models/Event.js')
 const { addEvent, viewEvents, updateEvent, getEventById, deleteEvent } = require('../controller/eventController.js')
 
 router.get('/',viewEvents)
--- Event-management-system/server/server.js
+++ Event-management-system/server/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db.js')
+const eventRoutes = require('./routes/eventRoutes.js')
+const userRoutes = require('./routes/userRoutes.js')
+const registrationRoutes = require('./routes/registrationRoutes.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 connectDB()
+
+app.use('/api/events', eventRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/registrations', registrationRoutes);
 
 app.listen(5000, () => console.log('Server port running in 5000'))
